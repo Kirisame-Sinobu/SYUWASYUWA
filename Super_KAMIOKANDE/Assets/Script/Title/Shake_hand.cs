b100c8f827709204fede576f4874b880be5ffed8
Taiju Okuda <hw17a042@oecu.jp>
2019-10-22 02:25:40 +0900

    しゅわしゅわ

---
+++ Super_KAMIOKANDE/Assets/Script/Title/Shake_hand.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shake_hand : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 befor_shake;
+    Vector3 after_shake;
+
+    Vector3 is_shake;
+
+    void Start()
+    {
+        after_shake = Input.acceleration;
+        befor_shake = after_shake;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        after_shake = Input.acceleration;
+
+        is_shake = after_shake - befor_shake;
+        if(is_shake.magnitude >= 1)
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        befor_shake = after_shake;
+
+    }
+}
