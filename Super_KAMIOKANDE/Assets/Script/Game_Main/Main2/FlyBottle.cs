e5669237725a147716b4f0044f951f37652a59b0
Yamanaka Tatsuki <hw17a201@oecu.jp>
2019-10-22 02:40:17 +0900

    できちゃった⭐︎

---
+++ Super_KAMIOKANDE/Assets/Script/Game_Main/Main2/FlyBottle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyBottle : MonoBehaviour
+{
+
+    [SerializeField] private float leftLimit;
+    [SerializeField] private float rightLimit;
+    [SerializeField] private float speed;
+    [SerializeField] private float riseSpeed;
+    public static float shakePow;
+
+    void Update()
+    {
+        Rise();
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+
+        //if(dir.sqrMagnitude > 1) {
+        //    dir.Normalize();
+        //}
+
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+    
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x,leftLimit,
+            rightLimit), transform.position.y, transform.position.z);
+    }
+
+    public void Rise() {
+        this.transform.Translate(0,riseSpeed,0);
+    }
+    
+
+}
