c77b23f68b6738380e6cede5a4239699753a5858
Taiju Okuda <hw17a042@oecu.jp>
2019-10-22 00:37:04 +0900

    コードゴルフ

--- Super_KAMIOKANDE/Assets/Script/Title/Cola_wave.cs
+++ Super_KAMIOKANDE/Assets/Script/Title/Cola_wave.cs
@@ -12,17 +12,29 @@ public class Cola_wave : MonoBehaviour
 
     float minAngle = -30.0f, maxAngle = 30.0f;
     float now_time;
-    float wave_time = 2.0f;
+    float wave_time = 1.0f;
+    bool is_wave = true;
 
     // Update is called once per frame
     void Update()
     {
-        now_time += Time.deltaTime;
-        float angle = Mathf.LerpAngle(minAngle, maxAngle, Mathf.Sin(now_time / wave_time * Mathf.PI));
+        //if (is_wave)
+        //{
+            now_time += Time.deltaTime;
+        //}
+        //else
+        //{
+        //    now_time -= Time.deltaTime;
+        //}
+        float angle = Mathf.LerpAngle(minAngle, maxAngle, (1 + Mathf.Sin((now_time /wave_time) * Mathf.PI))/2.0f);
         transform.eulerAngles = new Vector3(0, 0, angle);
-        if (now_time >= wave_time)
-        {
-            now_time = 0;
-        }
+        //if (now_time >= wave_time)
+        //{
+        //    is_wave = false;
+        //}
+        //if(now_time <= 0)
+        //{
+        //    is_wave = true;
+        //}
     }
 }
