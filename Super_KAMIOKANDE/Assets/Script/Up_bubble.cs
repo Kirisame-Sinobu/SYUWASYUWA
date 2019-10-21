4e33b7ae02612762d855c1087db1ed42b1633b63
Taiju Okuda <hw17a042@oecu.jp>
2019-10-21 23:22:48 +0900

    ßç®ˆπ† ©ƒ∂˙˚¬˙∆˚ƒ˚∆˙¨¥ƒˆ´∆˚¬ç`///???

---
+++ Super_KAMIOKANDE/Assets/Script/Up_bubble.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Up_bubble : MonoBehaviour
+{
+
+    float Up_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Up_speed = Random.Range(0.2f, 1.0f);
+        //Debug.Log(Up_speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 hoge = gameObject.transform.position;
+        hoge.y += Up_speed * 0.05f;
+        gameObject.transform.position = hoge;
+        if (hoge.y > 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Super_KAMIOKANDE/Assets/Script/criate_bubble.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class criate_bubble : MonoBehaviour
+{
+
+    public GameObject bubble;
+
+    float now_time = 0.0f;
+    float criate_time = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        now_time = criate_time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        now_time += Time.deltaTime;
+        GameObject temp;
+        if(now_time > criate_time)
+        {
+            temp = Instantiate(bubble, new Vector3(Random.Range(-10.0f,10.0f), -6, 0),Quaternion.identity);
+            float temp_size = Random.Range(0.1f, 0.7f);
+            temp.transform.localScale = new Vector3(temp_size,temp_size,temp_size);
+            Material temp_mate;
+            temp_mate = temp.GetComponent<Renderer>().material;
+            temp_mate.SetFloat("_Wave", Random.Range(0.1f,1.5f));
+            temp_mate.SetFloat("_Speed", Random.Range(5.0f, 15.0f));
+            //Debug.Log(temp_mate.GetFloat("_Speed"));
+            temp_mate.SetFloat("_Speed", Random.Range(0.1f, 3.0f));
+            temp.GetComponent<Renderer>().material = temp_mate;
+			now_time = 0;
+		}
+    }
+}
