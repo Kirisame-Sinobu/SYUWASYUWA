b0c8adf5ba07add0e1680a13a4f73f56a4c07001
Taiju Okuda <hw17a042@oecu.jp>
2019-10-22 01:06:16 +0900

    shake_title!

---
+++ Super_KAMIOKANDE/Assets/Script/Title/Gettan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gettan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 start_pos;
+    void Start()
+    {
+        start_pos = this.transform.position;
+    }
+
+    float minAngle = 0f, maxAngle = 180.0f;
+    float now_time;
+    float wave_time = 1.0f;
+    bool is_wave = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        now_time += Time.deltaTime * Random.Range(0, 100.0f);
+        float angle = Mathf.LerpAngle(minAngle, maxAngle, (1 + Mathf.Sin((now_time / wave_time) * Mathf.PI)) / 2.0f);
+        transform.eulerAngles = new Vector3(0, 0, angle);
+        transform.position = new Vector3(0, angle * 0.02f, 0) + start_pos;
+    }
+}
--- Super_KAMIOKANDE/Assets/Script/Title/Title.cs
+++ Super_KAMIOKANDE/Assets/Script/Title/Title.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cola_wave : MonoBehaviour
+public class Title : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3 start_pos;
@@ -12,20 +12,20 @@ public class Cola_wave : MonoBehaviour
         start_pos = this.transform.position;
     }
 
-    float minAngle = -30.0f, maxAngle = 30.0f;
+    float minAngle = -5.0f, maxAngle = 5.0f;
     float now_time;
-    float wave_time = 0.6f;
+    float wave_time = 0.3f;
     bool is_wave = true;
 
     // Update is called once per frame
     void Update()
     {
+
         now_time += Time.deltaTime;
 
-        float angle = Mathf.LerpAngle(minAngle, maxAngle, (1 + Mathf.Sin((now_time /wave_time) * Mathf.PI))/2.0f);
+        float angle = Mathf.LerpAngle(minAngle, maxAngle, (1 + Mathf.Sin((now_time / wave_time) * Mathf.PI)) / 2.0f);
         transform.eulerAngles = new Vector3(0, 0, angle);
-        float pos = Mathf.LerpAngle(minAngle, maxAngle, (1 + Mathf.Cos((now_time / wave_time) * Mathf.PI)) / 2.0f);
-        transform.position = new Vector3(0, pos * 0.02f, 0) + start_pos;
+        transform.position = new Vector3(0, angle * 0.01f, 0) + start_pos;
 
     }
 }
