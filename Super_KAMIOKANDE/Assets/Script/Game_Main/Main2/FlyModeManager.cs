0b3c660a83149f71ef29cea0bc87129c14c6b23f
Yamanaka Tatsuki <hw17a201@oecu.jp>
2019-10-22 04:39:53 +0900

    おやすみzzZ

---
+++ Super_KAMIOKANDE/Assets/Script/Game_Main/Main2/FlyModeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyModeManager : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private float pow;
+    void Start()
+    {
+        pow = FlyBottle.shakePow;
+        slider.value = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //slider.value -= 0.00001f;
+        
+    }
+}
